f71fbb601a47eaf9fe48baa1f2d98e8130f75ead
Zephyres <zephyr.ocn@gmail.com>
2015-12-01 12:36:31 +0800

    Updated Club project

--- hw-bluej/hw4e-club/Club.java
+++ hw-bluej/hw4e-club/Club.java
@@ -23,4 +23,19 @@ public class Club {
     public int numberOfMembers() {
         return members.size();
     }
+    
+    public ArrayList<Membership> purge(int month, int year) {
+       ArrayList<Membership> p = new ArrayList<Membership>();
+        
+       if(month < 1 || month > 12) {
+           System.err.printf("Error: month must be in range 1-12!\n");
+           return null;
+       }
+        
+       for(Membership m : members)
+           if(m.getMonth() == month && m.getYear() == year)
+               p.add(m);
+               
+       return p;
+    }
 }
