895886b45773de293ca8a795b8f1624178a13bec
Zephyres <zephyr.ocn@gmail.com>
2016-02-19 15:00:38 +0800

    Added Dirac

---
+++ hw-netbeans/hw5e-polymorphism/src/Dirac.java
@@ -0,0 +1,13 @@
+public class Dirac extends GreatPerson {
+
+	@Override
+	public void printAwesomeContributionsMayBeMoreThanOne() {
+		System.out.println("Relativistic quantum mechanics, positron prediction");
+	}
+
+	@Override
+	public double getAwesomenessFactor() {
+		return 9000;
+	}
+	
+}
